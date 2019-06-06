71ae110da9fadde257be19b3d1bb49b734a5c400
Dinh Chau <Dinh.Chau@home.cas.de>
2019-06-06 14:37:39 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,65 @@
+var container = document.querySelector('.container');
+var cardTemplate = document.querySelector('.cardTemplate');
+var cards = {};
+var selected = document.querySelector('#selectedCityToAdd');
+var url = 'http://api.openweathermap.org/data/2.5/weather?APPID=3acf6a94d226fdbd6fffc6d6ff885385&units=metric&q=';
+var selectedCities = [];
+
+document.querySelector('#addBut').addEventListener('click', function () {
+    var selectedCity = selected.options[selected.selectedIndex].textContent;
+    if (!selectedCities.includes(selectedCity)) {
+        selectedCities.push(selectedCity);
+    }
+    getForcastOfCity(selectedCity);
+    saveSelectedCities();
+})
+
+saveSelectedCities = function() {
+    window.localforage.setItem('selectedCities', selectedCities);
+    window.localforage.getItem('selectedCities', function(cities) {
+        console.log(cities);
+    });
+}
+
+getForcastOfCity = function(city) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var data = JSON.parse(request.response);
+                updateCard(data);
+            }
+        }
+    }
+    request.open('GET', url + city);
+    request.send();
+}
+
+
+
+updateCard = function (data) {
+    var card = cards[data.name];
+
+    if (!card) {
+        var card = cardTemplate.cloneNode(true);
+        card.removeAttribute('hidden');
+        card.querySelector('.location').textContent = data.name;
+        container.appendChild(card);
+        cards[data.name] = card;
+    }
+
+    card.querySelector('.current .temperature .value').textContent = Math.round(data.main.temp);
+    var date = new Date(data.dt*1000);
+    card.querySelector('.date').textContent = date;
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.localforage.getItem('selectedCities', function(err, cities) {
+        if (cities) {
+            selectedCities = cities;
+            selectedCities.forEach(city => getForcastOfCity(city));
+        }
+    });
+})
